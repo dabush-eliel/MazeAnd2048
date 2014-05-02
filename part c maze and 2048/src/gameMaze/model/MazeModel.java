98430273a5e652a04b04f2dd7d717c5f8cff717c
Eliel <dlieldx@gmail.com>
2014-05-02 11:00:24 +0300

    create setPosition for maze, still not in use. wana check how to work with maze&State from Matala 1. would be easier for the next mission - using aStar algorithm.

--- part c maze and 2048/src/gameMaze/model/MazeModel.java
+++ part c maze and 2048/src/gameMaze/model/MazeModel.java
@@ -1,14 +1,16 @@
 package gameMaze.model;
 
+import java.util.ArrayList;
 import java.util.Observable;
+import java.util.Random;
 import java.util.Stack;
 
 public class MazeModel extends Observable implements Model{
-	final private int rows 					= 12;					// NEED TO SET	
-	final private int columns 				= 12;					// NEED TO SET	
+	final private int rows					= 12;					// NEED TO SET	rows + 2
+	final private int columns 				= 12;					// NEED TO SET	columns + 2
 	private int walls[][]					= {{6,4},{5,5},{7,7}};		// NEED TO SET	
 	private int maze[][]					= new int[rows][columns];
-	// we use int array of 2 num's like a point - .[0] = x , .[1] = y
+	// we use int array of 2 num's - s[0] = row , s[1] = column
 	private int[] start_s 					= new int[2]; 			// NEED TO SET
 	private int[] goal_s					= new int[2]; 			// NEED TO SET
 	private int[] mouse						= new int[2];			// current position
@@ -17,8 +19,45 @@ public class MazeModel extends Observable implements Model{
 	private Stack <Integer>	old_score		= new Stack <Integer>();
 	private Stack <int [][]> old_states		= new Stack <int [][]>();
 	
+/*	public MazeModel(int r, int c){
+		// we want to get the size of the maze from the player
+		this.rows = r;
+		this.columns = c;
+		this.maze = new int[rows+2][columns+2]; 	// +2 because we set a black frame of '-1'-s 
+		initGame();
+	}
+	*/
 	public MazeModel(){
 		initGame();
+	}
+	
+	// find a random spot for the mouse & goal & walls 
+	private void setPositions(){
+		int numOfWalls 	= (rows*columns)/3;
+		walls 			= new int[numOfWalls][2];
+		
+		// two arrays  of zero's. to mark where we already set a wall
+		int r,c;
+		Random rand = new Random();
+	//	Random randC = new Random();
+
+		int rArr[]	= new int[rows];
+		int cArr[] 	= new int[columns];
+		
+		for(int i=0;i<numOfWalls;i++){
+			
+			r = rand.nextInt(rows);
+			c = rand.nextInt(columns);
+			if(rArr[r] == 0){
+				walls[i][0] = r;
+				walls[i][1] = c;
+				rArr[r] = 1;				
+			}
+			
+			
+			
+
+		}
 	}
 	
 	@Override
@@ -27,6 +66,10 @@ public class MazeModel extends Observable implements Model{
 		start_s[1]	= 10;
 		goal_s[0] 	= 3;
 		goal_s[1] 	= 1;
+		
+		// method to find automatically what will be the start & goal & walls for the maze that user asked for
+//		setPositions();	
+		
 		// set maze start = 1, goal = 2, walls = -1, allowed moves = 0 / 2 -> goal;
 		// we also set the boundaries to be -1 instead of been out of the array limits
 		for(int i=0; i<maze.length; i++){
@@ -34,16 +77,19 @@ public class MazeModel extends Observable implements Model{
 				maze[i][j] = 0;
 			}
 		}
+		
 		maze[start_s[0]][start_s[1]] = 1;
 		maze[goal_s[0]][goal_s[1]] = 2;
+		
 		for(int[] w : walls){
 			maze[w[0]][w[1]] = -1;
 		}
+		
 		// current mouse state
 		mouse[0] = start_s[0];
 		mouse[1] = start_s[1];
 		
-		// boundaries
+		// boundaries - set it to '-1' to get a black frame
 		for(int j=0; j<maze[0].length; j++){
 			maze[0][j] 			= -1;
 			maze[j][0] 			= -1;
@@ -106,6 +152,7 @@ public class MazeModel extends Observable implements Model{
 			notifyObservers();
 			break;
 		case 11:
+			
 			break;
 		case 12:
 			break;
