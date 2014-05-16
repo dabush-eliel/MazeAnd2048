39621d9910f7996a1b6b011e668c2e2fde505db8
Eliel <dlieldx@gmail.com>
2014-05-16 18:29:05 +0300

    start work on myAlgo

--- part c maze and 2048/src/algorithms/MyAlgo.java
+++ part c maze and 2048/src/algorithms/MyAlgo.java
@@ -9,15 +9,29 @@ public class MyAlgo implements Solver, Serializable{
 	 */
 	private static final long serialVersionUID = 8168102632311639749L;
 
+	
+	public MyAlgo(){
+		
+	}
+	
 	@Override
 	public int calculator(Model model) {
 		
+		int [][]data = copyArray(model.getData());
 		
 		
 		return 0;
 	}
 	
 	
-	
+	private int[][] copyArray (int[][] arr){
+		int[][] copiedArr = new int[arr.length][arr[0].length];
+		for(int i = 0 ; i < arr.length; i++){
+			for(int j = 0 ; j < arr[0].length; j++){
+				copiedArr[i][j] = arr[i][j];
+			}
+		}
+		return copiedArr;
+	}
 	
 }
